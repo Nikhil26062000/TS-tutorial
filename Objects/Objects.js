f15d81436a5473195fa8c9e654c6fbed90ead429
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-05-31 01:01:43 +0530

    TS tutorial/Objects/Objects.ts

---
+++ Objects/Objects.js
@@ -0,0 +1,10 @@
+var person = {
+    name: "Nikhil",
+    age: 28,
+    isStudent: false,
+    address: {
+        city: "XYZ",
+        country: "India"
+    }
+};
+console.log(person);
---
+++ Objects/Objects.ts
@@ -0,0 +1,20 @@
+
+const person:{
+    name : string;
+    age : number;
+    isStudent : boolean;
+    address : {
+        city : string;
+        country : string;
+    }
+} = {
+    name : "Nikhil",
+    age : 28,
+    isStudent : false,
+    address : {
+        city : "XYZ",
+        country : "India"
+    }
+}
+
+console.log(person);
