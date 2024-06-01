30909e390649c33e9eed8489969a9e8146e341b2
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-06-01 20:39:41 +0530

    Objects/unnnionIntersectionn.ts

--- Objects/unnnionIntersectionn.js
+++ Objects/unnnionIntersectionn.js
@@ -1,16 +1,39 @@
-// Union 
+// Union
 //?  ------------------------------------------------------------------------------ >
-//?      🔥 Question : If the input value is number then return its double 
+//?      🔥 Question : If the input value is number then return its double
 //?      😱 if it is string return it in uppercase
 //?  ------------------------------------------------------------------------------->
 var convertInto = function (value) {
-    if (typeof value === 'number') {
+    if (typeof value === "number") {
         return value * 2;
     }
-    if (typeof value === 'string') {
+    if (typeof value === "string") {
         return value.toUpperCase();
     }
     throw new Error("Invalid input value");
 };
 console.log(convertInto(2));
 console.log(convertInto("nikhil"));
+var employee = {
+    name: "nikhil",
+    age: 30,
+    empId: 2022,
+};
+var officeEmployee = {
+    name: "Nikk",
+    age: 34,
+    empId: 2343,
+    departmentId: 2122,
+};
+var User1 = {
+    name: "Nikhil raj",
+    age: 28
+};
+var MyLocation = {
+    city: "XYX",
+    country: "India"
+};
+var MyInroduction = function (User1, MyLocation) {
+    return "Hello My Name is ".concat(User1.name, " and I am ").concat(User1.age, " and I live in ").concat(MyLocation.city, " which is in ").concat(MyLocation.country);
+};
+console.log(MyInroduction(User1, MyLocation));
--- Objects/unnnionIntersectionn.ts
+++ Objects/unnnionIntersectionn.ts
@@ -1,49 +1,44 @@
-
-
-// Union 
+// Union
 
 //?  ------------------------------------------------------------------------------ >
-//?      🔥 Question : If the input value is number then return its double 
+//?      🔥 Question : If the input value is number then return its double
 //?      😱 if it is string return it in uppercase
 //?  ------------------------------------------------------------------------------->
 
-const convertInto = (value:string|number):string|number => {
-    if(typeof value === 'number'){
-        return value * 2
-    }
-    if(typeof value === 'string'){
-        return value.toUpperCase()
-    }
-    throw new Error(`Invalid input value`)
-}
+const convertInto = (value: string | number): string | number => {
+  if (typeof value === "number") {
+    return value * 2;
+  }
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  }
+  throw new Error(`Invalid input value`);
+};
 
 console.log(convertInto(2));
 console.log(convertInto("nikhil"));
-
 
 //?  ------------------------------------------------------------------------------ >
 //?      😁 Another example of Union
 //?  ------------------------------------------------------------------------------->
 
-
 type Person = {
-    name: string;
-    age: number;
-}
+  name: string;
+  age: number;
+};
 
 type Employee = {
-    empId: number;
-    departmentId: number;
-}
+  empId: number;
+  departmentId: number;
+};
 
-type EmployeeDetails = Person | Employee 
+type EmployeeDetails = Person | Employee;
 
-const employee:EmployeeDetails = {
-    name:"nikhil",
-    age:30,
-    empId:2022
-
-}
+const employee: EmployeeDetails = {
+  name: "nikhil",
+  age: 30,
+  empId: 2022,
+};
 
 //! note : In union atleast we have to write all the data for atleast one type . either Person or Employee or both but we have to write all the field of that type.
 
@@ -54,8 +49,45 @@ const employee:EmployeeDetails = {
 
 //!    }  here i am writing name ,age,empId and this is fine . here i have written all the field from Person and only one field "empId" from Employee  and this is fine but if i write only one field from Person type and only one field from Employee type then this is not correct in Union . I HAVE TO  WRITE ALL THE FIELDS OF ATLEAST ONE OF THE TYPE
 
+// Intersection
 
+type officeEmployeedetails = Person & Employee;
 
+const officeEmployee: officeEmployeedetails = {
+  name: "Nikk",
+  age: 34,
+  empId: 2343,
+  departmentId: 2122,
+};
 
+//! note : In Intersection we have to write field from both type . If i dont write any one field from any of the type then this is not acceptable in Intersection
 
+//? ------------------------------------------------->
+//?     😘💖 Example Question on Intersection
+//? ------------------------------------------------->
 
+type MyUser = {
+  name: string;
+  age: number;
+};
+
+type MyLocation = {
+  city: string;
+  country: string;
+};
+
+const User1: MyUser = {
+  name: "Nikhil raj",
+  age: 28,
+};
+
+const MyLocation: MyLocation = {
+  city: "XYX",
+  country: "India",
+};
+
+const MyInroduction = (User1: MyUser, MyLocation: MyLocation): string => {
+  return `Hello My Name is ${User1.name} and I am ${User1.age} and I live in ${MyLocation.city} which is in ${MyLocation.country}`;
+};
+
+console.log(MyInroduction(User1, MyLocation));
