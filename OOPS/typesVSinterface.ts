5c0555dbebfd923f6a46b5dbae381a820001c9f3
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-06-06 20:51:57 +0530

    TS tutorial/OOPS/typesVSinterface.ts

---
+++ OOPS/typesVSinterface.ts
@@ -0,0 +1,8 @@
+
+
+// If we have to use Union and Intersection the use types
+// if we have to use objects and class then use interface
+
+
+// we can give two interface same name
+// but can't do in types
